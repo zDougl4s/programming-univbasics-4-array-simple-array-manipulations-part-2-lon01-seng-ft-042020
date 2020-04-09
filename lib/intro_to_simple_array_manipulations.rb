0f49e2adfb2dc12cffd418c90c49602357650407
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-09 02:08:27 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+
+def using_concat (array,new_array)
+  array.concat(new_array)
+end
+
+def using_insert(array,element)
+  array.insert(4,element)
+end
+
+def using_uniq (array)
+  array.uniq
+end
+
+def using_flatten (array)
+  array.flatten
+end
+
+def using_delete(array,string)
+  array.delete(string)
+end
+
+def using_delete_at(array,n)
+  array.delete_at(n)
+end
