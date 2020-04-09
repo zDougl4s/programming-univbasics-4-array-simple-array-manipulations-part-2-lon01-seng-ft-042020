cd0dfd9b674fe03c8a77d1340b2257387abf8645
Douglas Manuel Barco Martin <douglas@dobuilding.co.uk>
2020-04-09 01:52:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,6 @@ def using_concat (array,new_array)
   array.concat(new_array)
 end
 
-def using_inser(array,element)
-  array.inset(element)
-end 
+def using_insert(array,element)
+  array.insert(element)
+end
